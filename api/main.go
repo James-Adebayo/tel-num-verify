41ca33e344431095aa296b1bc51665984bdd461a
James Adebayo <redbarcompany+render@gmail.com>
2026-08-02 21:53:09 +0100

    Update file server path for index.html

--- api/main.go
+++ api/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.FileServer(http.Dir("./public/index.html"))
+		http.FileServer(http.Dir("../index.html"))
 	})
 	http.HandleFunc("/validate", controller.ValidateController)
 	// fmt.Println("Server started on http://localhost:8080/")
