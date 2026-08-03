91a6e50f9b0ad3714f898bc39b8283b9d99dfd57
James <redbarcompany+render@gmail.com>
2026-08-03 16:22:16 -0700

    updated designs

--- api/index.go
+++ api/index.go
@@ -9,12 +9,15 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	mux := http.NewServeMux()
 
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	mux.HandleFunc("/validate", controller.ValidateController)
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
 
 	log.Println("Validate route reached")
-	mux.HandleFunc("/validate", controller.ValidateController)
 
 	mux.ServeHTTP(w, r)
 }
