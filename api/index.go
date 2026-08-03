b1bd8fc5636e905af9cee65f4160a2bd65071764
James <redbarcompany+render@gmail.com>
2026-08-03 07:40:34 -0700

    updated index.go and index.html

--- api/index.go
+++ api/index.go
@@ -9,7 +9,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		http.ServeFile(w, r, "../index.html")
 	})
 
 	mux.HandleFunc("/validate", controller.ValidateController)
